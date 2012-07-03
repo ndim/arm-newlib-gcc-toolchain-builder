f1527f2eb2e6e13de8dc8e70d2f4bb7722a5b152
Hans Ulrich Niedermann <hun@n-dimensional.de>
2012-07-03 21:38:38 +0200

    environ.sh: Quote var definitions

--- environ.sh
+++ environ.sh
@@ -1,4 +1,4 @@
-export TARGET=arm-none-eabi
-export PREFIX=$HOME/DevelToolbin/binaries/armThumb-4.7.1
+export TARGET="arm-none-eabi"
+export PREFIX="$HOME/DevelToolbin/binaries/armThumb-4.7.1"
 export PATH="$PREFIX/bin:$PATH"
-export BUILDSOURCES=`pwd`
+export BUILDSOURCES="$PWD"
